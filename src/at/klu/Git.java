143edc4ec5a8d665dc4c80509b1cc204e6f65386
Chris <christian.gamlinger@gmx.at>
2013-01-09 19:56:06 +0100

    2nd Commit

--- src/at/klu/Git.java
+++ src/at/klu/Git.java
@@ -8,6 +8,7 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My name is Chris");
+		System.out.println("My name is Chris");
 
 	}
 
